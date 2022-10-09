cacec344389c3de2f0aafb76da94b42a5af5e738
dimeji <gbadeyankashaban24@gmail.com>
2022-10-09 17:26:50 -0400

    stash code to review changes

--- js/frontpage.js
+++ js/frontpage.js
@@ -1,16 +1,25 @@
 let play = document.getElementById("play");
 let about = document.getElementById("about");
+let store = document.getElementById("store");
+let otherGames = document.getElementById("other");
 play.addEventListener("click", function () {
-//   let p =new Promise((resolve,reject)=>{
-//     setTimeout(()=>{
-//      return resolve("done")
-//     },2000);
-//   });     
-//  p.then((data)=>{
-//   console.log(data);
+  //   let p =new Promise((resolve,reject)=>{
+  //     setTimeout(()=>{
+  //      return resolve("done")
+  //     },2000);
+  //   });
+  //  p.then((data)=>{
+  //   console.log(data);
   window.location.href = "index.html";
-//  }).catch((err)=>{
-//    console.log(err);
-   
-//  })
-})
+  //  }).catch((err)=>{
+  //    console.log(err);
+
+  //  })
+});
+store.addEventListener("click", function () {
+  window.location.href =
+    "https://www.microsoft.com/en-in/store/top-free/apps/pc";
+});
+otherGames.addEventListener("click", function () {
+  window.location.href = "https://www.microsoft.com/en-in/store/games";
+});
